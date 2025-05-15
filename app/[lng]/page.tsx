b0cbb01ad17e0809ca998ce3e2a36946e26e409f
Jan <js485@hdm-stuttgart.de>
2025-05-15 20:23:35 +0200

    build: fix type error

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -1,6 +1,10 @@
 import GamePage from "@/components/GamePage";
 
-export default async function Page({ params }: { params: { lng: string } }) {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ lng: string }>;
+}) {
   const { lng } = await params;
   return <GamePage lng={lng} />;
 }
